6f89251066485f1e8954d637cccc68616394c9bd
cRefused <itaos@yandex.ru>
2024-07-07 10:43:10 +0500

    ...

--- s2_practic/2_3_003/repair_journal/app_rj/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppRJConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_rj'
-    verbose_name = "Журнал ремонта"
